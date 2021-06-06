942527e1913571926bb2c35310914679ae48591b
farhad0306 <71126444+farhad0306@users.noreply.github.com>
2021-06-06 23:09:40 +0200

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,39 +1,39 @@
-const snus = document.getElementById('snus');
+const snus = document.getElementById('snus'); // Returns a reference to the first object with the specified value of the ID attribute.
 const rock = document.getElementById('rock');
 const score = document.getElementById('score');
 
 
 //creates the jump animation//
 function jump() {
-    snus.classList.add('jump-animation');
-    setTimeout(() => {
-       snus.classList.remove('jump-animation');
-    }, 500); 
+    snus.classList.add('jump-animation'); // Attaches the jump animation to the class every time we jump
+    setTimeout(() => { 
+       snus.classList.remove('jump-animation'); // here it removes the jump animation after a specific time
+    }, 500); // the time is 500 ms
 }
 // Attaches the jump function to space//
-document.addEventListener('keypress', () => {
-    if (!snus.classList.contains('jump-animation')) {
+document.addEventListener('keypress', () => {  //adds the event below
+    if (!snus.classList.contains('jump-animation')) { // Creates so you cant jump until the animation is completed
     jump();
     }
 })
 
 setInterval(() => {
     score.innerText++;
-   const snusTop = parseInt(window.getComputedStyle(snus)
-   .getPropertyValue('top'));
-   const rockLeft = parseInt(window.getComputedStyle(rock)
-   .getPropertyValue('left'));
+   const snusTop = parseInt(window.getComputedStyle(snus) // Gets the information out of the snus element which we attached to the css document.
+   .getPropertyValue('top')); // Gets the "top" property out of the CSS file
+   const rockLeft = parseInt(window.getComputedStyle(rock) // Gets the information out of the rock element which we attached to the css document.
+   .getPropertyValue('left')); // Gets the "left" property out of the CSS file
    
-   if (rockLeft < 0) {
-    rock.style.display = "none";
-} else {
-    rock.style.display = '';
+   if (rockLeft < 0) { // if the rock passes a negative value the becomes invisible
+    rock.style.display = "none"; // makes it not showing
+} else { // when it has dissapeared
+    rock.style.display = '';  // it makes it reappear again
 
 }
 
-if (rockLeft < 50 && rockLeft > 0 && snusTop > 100) {
+if (rockLeft < 50 && rockLeft > 0 && snusTop > 100) { // if the character is close to those cordinates which is where it would meet the stone
     alert("                                       You got a score of: " + score.innerText +
-      "\n\n                                               Play again?");
-location.reload();
+      "\n\n                                               Play again?"); // we would alert the user with the message that the game is over.
+location.reload(); // and refreshes the page when you click ok on the alert.
 }
-}, 50);
+}, 50); // all this gets updated every 50 ms.
